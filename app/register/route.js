bde0abba57526b6d33ecb17eacc6dfb108e15883
Brian Dear <superacidjax@me.com>
2015-08-22 15:18:24 +0200

    cleans up syntax

--- app/register/route.js
+++ app/register/route.js
@@ -8,8 +8,8 @@ export default Ember.Route.extend({
       });
       user.save().then(() => {
         console.log('save successful');
-        the.controller.set('message', 'A new user with the name "'
-          + this.controller.get('name') + '" was added!');
+        this.controller.set('message', 'A new user with the name "' +
+          this.controller.get('name') + '" was added!');
         this.controller.set('name', null);
       }, function() {
         console.log('save failed');
